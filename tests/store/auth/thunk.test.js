fd4ffaaaa8144ad70e624d5af5626b1dc69c992e
Angelica <angelica.piedrahita@wasolutions.co>
2023-01-18 11:53:08 -0500

    pruebas sobre thunks (login y logout)

--- tests/store/auth/thunk.test.js
+++ tests/store/auth/thunk.test.js
@@ -1,4 +1,6 @@
-import { chekingCredentials } from "../../../src/store/auth"
+import { singInWithGoogle } from "../../../src/firebase"
+import { chekingCredentials, login, logout, startGoogleSignIn } from "../../../src/store/auth"
+import { demoUser } from "../../fixture/authFixtures"
 
 jest.mock('../../../src/firebase/providers')
 
@@ -12,4 +14,26 @@ describe('pruebas sobre authThunks', () => {
         await chekingCredentials()(dispatch)
         expect(dispatch).toHaveBeenCalledWith(chekingCredentials())
     })
-})  
+
+    test('startGoogleSignIn debe de llamar chejing credentials y login -exito', async () => {
+
+        const loginData = { ok: true, ...demoUser }
+
+        await singInWithGoogle.mockResolveValue(loginData)
+        await startGoogleSignIn()(dispatch)
+
+        expect(dispatch).toHaveBeenCalledWith(chekingCredentials())
+        expect(dispatch).toHaveBeenCalledWith(login())
+    })
+
+    test('startGoogleSignIn debe de llamar chejing credentials y logout -error', async () => {
+
+        const loginData = { ok: false, errorMessage: 'un error en google' }
+
+        await singInWithGoogle.mockResolveValue(loginData)
+        await startGoogleSignIn()(dispatch)
+
+        expect(dispatch).toHaveBeenCalledWith(chekingCredentials())
+        expect(dispatch).toHaveBeenCalledWith(logout(loginData.errorMessage))
+    })
+})
